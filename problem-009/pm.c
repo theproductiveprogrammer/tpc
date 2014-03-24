c13934c3d7698ac4c637758d24d7c2d922752d4a
b_d <1browndragon@gmail.com>
2014-03-24 19:03:28 +0530

    Made pm.c easier to read

--- problem-009/pm.c
+++ problem-009/pm.c
@@ -3,6 +3,13 @@
 #define MAX_FSZ (1 * 1024 * 1024)
 #define MAX_DEPTH (1 * 1024 * 1024)
 
+#define CUR_INP (buf_i[ndx_i])
+#define CUR_PATH (pt[pt_i])
+#define CUR_PAT (buf_p[CUR_PATH])
+#define NEXT_PAT (buf_p[CUR_PATH+1])
+#define NEXT_NEXT_PAT (buf_p[CUR_PATH+2])
+#define NEW_PATH (pt[pt_limit++])
+
 int main (int argc, char* argv[])
 {
         int dump;
@@ -25,38 +32,27 @@ int main (int argc, char* argv[])
         patf = argv[2];
     }
 
-    /* read in pattern file */
     fpat = fopen (patf, "rb");
     sz_p = fread (buf_p, sizeof (char), MAX_FSZ, fpat);
     buf_p[sz_p] = 0;
-    /* and split into lines */
     for (p_p=buf_p;*p_p;p_p++) if (*p_p == '\n' || *p_p == '\r') *p_p = 0;
-
-    /* read in input file */
     finp = fopen (inputf, "rb");
     sz_i = fread (buf_i, sizeof (char), MAX_FSZ, finp);
     buf_i[sz_i] = 0;
-    /* and split into lines */
     for (p_i=buf_i;*p_i;p_i++) if (*p_i == '\n' || *p_i == '\r') *p_i = 0;
 
-    /* loop over patterns */
     for (p_p=buf_p,ndx_p=0;ndx_p <= sz_p;ndx_p++) {
         if (!buf_p[ndx_p]) {
-            /* found next line */
             if (*p_p) {
-                /* found a pattern in p_p */
                 pt_limit = 1;
                 pt[0] = p_p - buf_p;
                 for (p_i=buf_i,ndx_i=0;ndx_i <= sz_i;ndx_i++) {
-                    if (!buf_i[ndx_i]) {
-                        /* found next line */
+                    if (!CUR_INP) {
                         if (*p_i) {
-                            /* found an input in p_i */
                             for (pt_i = 0;pt_i < pt_limit;pt_i++) {
-                                if (pt[pt_i]!=-1) {
-                                    if (buf_p[pt[pt_i]] == '*') while (buf_p[pt[pt_i]] == '*') pt[pt_i]++;
-                                    if (!buf_p[pt[pt_i]]) {
-                                        /* found a matching path */
+                                if (CUR_PATH!=-1) {
+                                    if (CUR_PAT == '*') while (CUR_PAT == '*') CUR_PATH++;
+                                    if (!CUR_PAT) {
                                         num++;
                                         if (dump) printf ("%s\n", p_i);
                                         break;
@@ -65,46 +61,37 @@ int main (int argc, char* argv[])
                             }
                         }
                         p_i = buf_i + ndx_i + 1;
-                        /* reset paths */
                         pt_limit = 1;
                         pt[0] = p_p - buf_p;
                     }
                     else {
                         for (pt_i = pt_limit;pt_i > 0;) {
                             pt_i--;
-                            if (pt[pt_i] != -1) {
-                                if (!buf_p[pt[pt_i]]) pt[pt_i] = -1;
-                                else if (buf_p[pt[pt_i]] == '\\') {
-                                    if ((buf_p[pt[pt_i]+1] | 0x20) != (buf_i[ndx_i] | 0x20)) pt[pt_i] = -1;
-                                    else { /*printf ("c:%c,p:%c%c, inc+2\n", buf_i[ndx_i],buf_p[pt[pt_i]],buf_p[pt[pt_i]+1]);*/pt[pt_i] += 2;}
+                            if (CUR_PATH != -1) {
+                                if (!CUR_PAT) CUR_PATH = -1;
+                                else if (CUR_PAT == '\\') {
+                                    if ((NEXT_PAT | 0x20) != (CUR_INP | 0x20)) CUR_PATH = -1;
+                                    else CUR_PATH += 2;
                                 }
-                                else if (buf_p[pt[pt_i]] == '?') pt[pt_i]++;
-                                else if (buf_p[pt[pt_i]] == '*') {
-                                    while (buf_p[pt[pt_i]+1] == '*') pt[pt_i]++;
-                                    if (buf_p[pt[pt_i]+1] && ((buf_p[pt[pt_i]+1] | 0x20) == (buf_i[ndx_i+1] | 0x20))) {
-   /*                                   printf ("c:%c,p:%c,next->c:%c,p:%c inc 1\n", buf_i[ndx_i], buf_p[pt[pt_i]],
-                                                buf_i[ndx_i+1], buf_p[pt[pt_i]+1]);*/
-                                        pt[pt_limit++] = pt[pt_i]+1;
+                                else if (CUR_PAT == '?') CUR_PATH++;
+                                else if (CUR_PAT == '*') {
+                                    while (NEXT_PAT == '*') CUR_PATH++;
+                                    if (NEXT_PAT && ((NEXT_PAT | 0x20) == (buf_i[ndx_i+1] | 0x20))) {
+                                        NEW_PATH = CUR_PATH+1;
                                     }
-                                    if ((buf_p[pt[pt_i]+1] != '\\') && ((buf_p[pt[pt_i]+1] | 0x20) == (buf_i[ndx_i] | 0x20))) {
-   /*                                   printf ("c:%c,p:%c,next->c:%c,p:%c inc 2\n", buf_i[ndx_i], buf_p[pt[pt_i]],
-                                                buf_i[ndx_i+1], buf_p[pt[pt_i]+1]);*/
-                                        pt[pt_limit++] = pt[pt_i]+2;
+                                    if ((NEXT_PAT != '\\') && ((NEXT_PAT | 0x20) == (CUR_INP | 0x20))) {
+                                        NEW_PATH = CUR_PATH+2;
                                     }
-                                    else if (buf_p[pt[pt_i]+1] == '?') {
- /*                                     printf ("c:%c,p:%c,next->c:%c,p:%c inc+2\n", buf_i[ndx_i], buf_p[pt[pt_i]],
-                                                buf_i[ndx_i+1], buf_p[pt[pt_i]+1]);*/
-                                        pt[pt_limit++] = pt[pt_i]+2;
+                                    else if (NEXT_PAT == '?') {
+                                        NEW_PATH = CUR_PATH+2;
                                     }
-                                    else if ((buf_p[pt[pt_i]+1] == '\\') &&
-                                              (buf_p[pt[pt_i]+2] == buf_i[ndx_i])) {
- /*                                     printf ("c:%c,p:%c,next->c:%c,p:%c inc+3\n", buf_i[ndx_i], buf_p[pt[pt_i]],
-                                                buf_i[ndx_i+1], buf_p[pt[pt_i]+1]);*/
-                                        pt[pt_limit++] = pt[pt_i]+3;
+                                    else if ((NEXT_PAT == '\\') &&
+                                              (NEXT_NEXT_PAT == CUR_INP)) {
+                                        NEW_PATH = CUR_PATH+3;
                                     }
                                 }
-                                else if ((buf_p[pt[pt_i]] | 0x20) != (buf_i[ndx_i] | 0x20)) pt[pt_i] = -1;
-                                else { /*printf ("%c,%c\n", buf_p[pt[pt_i]], buf_i[ndx_i]);*/ pt[pt_i]++;}
+                                else if ((CUR_PAT | 0x20) != (CUR_INP | 0x20)) CUR_PATH = -1;
+                                else CUR_PATH++;
                             }
                         }
                     }
